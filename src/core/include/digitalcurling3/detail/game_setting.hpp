de0efbce7f6987d26921280c1365b81f972bb5e4
kuro1215 <128684921+kuro1215@users.noreply.github.com>
2025-11-28 18:48:35 +0900

    refactor(core)!: make GameSetting header-only
    Move the implementation of `GameSetting` from `game_setting.cpp` to `game_setting.hpp`.
    
    BREAKING CHANGE: The `game_setting.cpp` file has been removed.

--- src/core/include/digitalcurling3/detail/game_setting.hpp
+++ src/core/include/digitalcurling3/detail/game_setting.hpp
@@ -63,8 +63,25 @@ struct GameSetting {
 
 /// \cond Doxygen_Suppress
 // json
-void to_json(nlohmann::json &, GameSetting const&);
-void from_json(nlohmann::json const&, GameSetting &);
+inline void to_json(nlohmann::json& j, GameSetting const& v) {
+    j["max_end"] = v.max_end;
+    j["sheet_width"] = v.sheet_width;
+    j["five_rock_rule"] = v.five_rock_rule;
+    j["thinking_time"]["team0"] = v.thinking_time[0];
+    j["thinking_time"]["team1"] = v.thinking_time[1];
+    j["extra_end_thinking_time"]["team0"] = v.extra_end_thinking_time[0];
+    j["extra_end_thinking_time"]["team1"] = v.extra_end_thinking_time[1];
+}
+
+inline void from_json(nlohmann::json const& j, GameSetting& v) {
+    j.at("max_end").get_to(v.max_end);
+    j.at("sheet_width").get_to(v.sheet_width);
+    j.at("five_rock_rule").get_to(v.five_rock_rule);
+    j.at("thinking_time").at("team0").get_to(v.thinking_time[0]);
+    j.at("thinking_time").at("team1").get_to(v.thinking_time[1]);
+    j.at("extra_end_thinking_time").at("team0").get_to(v.extra_end_thinking_time[0]);
+    j.at("extra_end_thinking_time").at("team1").get_to(v.extra_end_thinking_time[1]);
+}
 /// \endcond
 
 } // namespace digitalcurling3
