5f5aaf5c5a79374b03c0e91ac134715324c60417
kuro1215 <128684921+kuro1215@users.noreply.github.com>
2025-12-25 22:50:17 +0900

    feat(core): add a method and operators to Vector2
    Added `SquaredLength()` method and equality operators to Vector2 struct.

--- src/core/include/digitalcurling/vector2.hpp
+++ src/core/include/digitalcurling/vector2.hpp
@@ -27,44 +27,11 @@ struct Vector2 {
     /// @param[in] y y座標
     constexpr Vector2(float x, float y) : x(x), y(y) {}
 
-    /// @brief ベクトルの加算を行う
-    /// @param[in] v ベクトル
-    /// @returns このベクトル自身
-    constexpr Vector2 & operator += (Vector2 v)
+    /// @brief ベクトルの長さの二乗を得る
+    /// @returns ベクトルの長さの二乗 (`x*x + y*y`)
+    constexpr float SquaredLength() const
     {
-        x += v.x;
-        y += v.y;
-        return *this;
-    }
-
-    /// @brief ベクトルの減算を行う
-    /// @param[in] v ベクトル
-    /// @returns このベクトル自身
-    constexpr Vector2 & operator -= (Vector2 v)
-    {
-        x -= v.x;
-        y -= v.y;
-        return *this;
-    }
-
-    /// @brief ベクトルにスカラー値を乗算する
-    /// @param[in] f スカラー値
-    /// @returns このベクトル自身
-    constexpr Vector2 & operator *= (float f)
-    {
-        x *= f;
-        y *= f;
-        return *this;
-    }
-
-    /// @brief ベクトルをスカラー値で除算する
-    /// @param[in] f スカラー値
-    /// @returns このベクトル自身
-    constexpr Vector2 & operator /= (float f)
-    {
-        x /= f;
-        y /= f;
-        return *this;
+        return x * x + y * y;
     }
 
     /// @brief ベクトルの長さを得る
@@ -73,66 +40,60 @@ struct Vector2 {
     {
         return std::hypot(x, y);
     }
+
+
+    /// @cond Doxygen_Suppress
+    // equation operators
+    friend constexpr bool operator == (Vector2 v1, Vector2 v2) {
+        return v1.x == v2.x && v1.y == v2.y;
+    }
+    friend constexpr bool operator != (Vector2 v1, Vector2 v2) {
+        return !(v1 == v2);
+    }
+
+    // arithmetic operators
+    constexpr Vector2 & operator += (Vector2 v) {
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+    constexpr Vector2 & operator -= (Vector2 v) {
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+    constexpr Vector2 & operator *= (float f) {
+        x *= f;
+        y *= f;
+        return *this;
+    }
+    constexpr Vector2 & operator /= (float f) {
+        x /= f;
+        y /= f;
+        return *this;
+    }
+
+    constexpr Vector2 operator-(Vector2 v) {
+        return { -v.x, -v.y };
+    }
+    friend constexpr Vector2 operator+(Vector2 v1, Vector2 v2) {
+        return { v1.x + v2.x, v1.y + v2.y };
+    }
+    friend constexpr Vector2 operator-(Vector2 v1, Vector2 v2) {
+        return { v1.x - v2.x, v1.y - v2.y };
+    }
+
+    friend constexpr Vector2 operator*(float f, Vector2 v) {
+        return { f * v.x, f * v.y };
+    }
+    friend constexpr Vector2 operator*(Vector2 v, float f) {
+        return f * v;
+    }
+    friend constexpr Vector2 operator/(Vector2 v, float f) {
+        return { v.x / f, v.y / f };
+    }
+    /// @endcond
 };
-
-/// @brief ベクトルを反転する
-/// @param[in] v ベクトル
-/// @returns @a -v
-/// @memberof Vector2
-constexpr Vector2 operator - (Vector2 v)
-{
-    return { -v.x, -v.y };
-}
-
-/// @brief ベクトル同士の加算を行う
-/// @param[in] v1 ベクトル1
-/// @param[in] v2 ベクトル2
-/// @returns @a v1+v2
-/// @memberof Vector2
-constexpr Vector2 operator + (Vector2 v1, Vector2 v2)
-{
-    return { v1.x + v2.x, v1.y + v2.y };
-}
-
-/// @brief ベクトル同士の減算を行う
-/// @param[in] v1 ベクトル1
-/// @param[in] v2 ベクトル2
-/// @returns @a v1-v2
-/// @memberof Vector2
-constexpr Vector2 operator - (Vector2 v1, Vector2 v2)
-{
-    return { v1.x - v2.x, v1.y - v2.y };
-}
-
-/// @brief ベクトルとスカラー値の乗算を行う
-/// @param[in] f スカラー値
-/// @param[in] v ベクトル
-/// @returns @a f*v
-/// @memberof Vector2
-constexpr Vector2 operator * (float f, Vector2 v)
-{
-    return { f * v.x, f * v.y };
-}
-
-/// @brief ベクトルとスカラー値の乗算を行う
-/// @param[in] v ベクトル
-/// @param[in] f スカラー値
-/// @returns @a v*f
-/// @memberof Vector2
-constexpr Vector2 operator * (Vector2 v, float f)
-{
-    return f * v;
-}
-
-/// @brief ベクトルとスカラー値の除算を行う
-/// @param[in] v ベクトル
-/// @param[in] f スカラー値
-/// @returns @a v*(1/f)
-/// @memberof Vector2
-constexpr Vector2 operator / (Vector2 v, float f)
-{
-    return { v.x / f, v.y / f };
-}
 
 
 /// @cond Doxygen_Suppress
