8c5d4c51991e122fc91d124a078a5bb22eb15d0d
kuro1215 <128684921+kuro1215@users.noreply.github.com>
2025-12-26 17:58:52 +0900

    refactor(core)!: introduce and adopt TeamValue<T>
    Change type of some fields in `GameState` and `GameSetting` to `TeamValue<T>` from `std::array<T, 2>`:
    - `GameState`: `thinking_time_remaining`
    - `GameSetting`: `thinking_time` and `extra_end_thinking_time`
    
    BREAKING CHANGE: Some field types in `GameState` and `GameSetting` have changed.

--- src/core/include/digitalcurling/team.hpp
+++ src/core/include/digitalcurling/team.hpp
@@ -2,14 +2,16 @@
 // SPDX-License-Identifier: MIT
 
 /// @file
-/// @brief Team を定義
+/// @brief Team, TeamValue を定義
 
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <cassert>
 #include <string>
 #include <type_traits>
+#include "digitalcurling/common.hpp"
 
 namespace digitalcurling {
 
@@ -69,6 +71,60 @@ inline std::underlying_type_t<T> ToUType(T value)
 /// @}
 
 
+/// @brief チームごとの値
+/// @tparam TValue 値の型
+template <typename TValue>
+struct TeamValue {
+public:
+    /// @brief チーム0の値
+    TValue team0;
+    /// @brief チーム1の値
+    TValue team1;
+
+    /// @brief コンストラクタ
+    TeamValue() : team0(), team1() {}
+
+    /// @brief コンストラクタ
+    /// @param[in] value 初期値(team0, team1ともに同じ値)
+    TeamValue(TValue value) : team0(value), team1(value) {}
+
+    /// @brief コンストラクタ
+    /// @param[in] team0 チーム0の値
+    /// @param[in] team1 チーム1の値
+    TeamValue(TValue team0, TValue team1) : team0(team0), team1(team1) {}
+
+    /// @brief コピーコンストラクタ
+    TeamValue(TeamValue<TValue> const&) = default;
+    /// @brief ムーブコンストラクタ
+    TeamValue(TeamValue<TValue>&&) = default;
+
+    /// @brief コピー演算子
+    TeamValue<TValue>& operator=(TeamValue<TValue> const&) = default;
+    /// @brief ムーブ演算子
+    TeamValue<TValue>& operator=(TeamValue<TValue>&&) = default;
+
+    /// @brief チームの値を取得する
+    /// @param[in] team チーム
+    TValue const& operator[](Team team) const {
+        switch (team) {
+            case Team::k0: return team0;
+            case Team::k1: return team1;
+            case Team::kInvalid:
+            default:
+                assert(false);
+                return team0;
+        }
+    }
+
+    /// @brief チームの値を取得する
+    ///
+    /// 1つ目の値がチーム0、2つ目の値がチーム1に対応します。
+    std::array<TValue, 2> ToArray() {
+        return { team0, team1 };
+    }
+};
+
+
 /// @cond Doxygen_Suppress
 // json
 NLOHMANN_JSON_SERIALIZE_ENUM(Team, {
@@ -76,6 +132,17 @@ NLOHMANN_JSON_SERIALIZE_ENUM(Team, {
     {Team::k1, "team1"},
     {Team::kInvalid, nullptr},
 })
+
+template <typename T>
+void to_json(nlohmann::json & j, digitalcurling::TeamValue<T> const& v) {
+    j["team0"] = v.team0;
+    j["team1"] = v.team1;
+}
+template <typename T>
+void from_json(nlohmann::json const& j, digitalcurling::TeamValue<T> & v) {
+    j.at("team0").get_to(v.team0);
+    j.at("team1").get_to(v.team1);
+}
 /// @endcond
 
 } // namespace digitalcurling
